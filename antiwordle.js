915d847b72e9e5cade014d4a4a6546b0383d130b
Rich Fromm <github@richard.fromm.name>
2022-02-16 13:55:26 -0800

    first working version
    simple text filtering, not a regexp
    likely to match any feeds discussing wordle
    not just actual wordle share posts

---
+++ antiwordle.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Anti Wordle
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Remove wordle posts from a facebook feed
+// @author       Rich Fromm
+// @match        http*://facebook.com/*
+// @match        http*://www.facebook.com/*
+// @require      http://code.jquery.com/jquery-latest.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Begin Anti Wordle setup");
+
+    // Filter the news feed every second
+    //setInterval(filter, 1000);
+    setInterval(filter, 5000);
+
+    console.log("End Anti Wordle setup");
+
+    function filter() {
+        console.log("Begin filter");
+
+        $("div[data-pagelet^='FeedUnit_']").filter($("div:contains('Wordle')")).remove();
+
+        console.log("End filter");
+    }
+})();
